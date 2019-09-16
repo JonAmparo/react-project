603b7a8e7046e6769c1e26b5dcc8a4cb7451c225
jonamparo <jonathanmamparo@gmail.com>
2019-09-16 11:22:43 -0400

    evens or odd setup

---
+++ evens-or-odds/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import App from "./components/App";
+import "./index.css";
+
+const DEFAULT_SETTINGS = {
+  gameStarted: false,
+  instructionsExpanded: false
+};
+
+const rootReducer = () => {
+  return DEFAULT_SETTINGS;
+};
+
+const store = createStore(rootReducer);
+
+console.log("store.getState()", store.getState());
+
+console.log("store", store);
+
+ReactDOM.render(<App />, document.getElementById("root"));
