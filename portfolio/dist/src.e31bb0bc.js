2cdd5f9804c0bb2d951b1b67fcc33231e7fe5b77
jonamparo <jonathanmamparo@gmail.com>
2019-09-27 08:50:42 -0400

    redux / connect react components

--- portfolio/dist/src.e31bb0bc.js
+++ portfolio/dist/src.e31bb0bc.js
@@ -30541,7 +30541,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57321" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57512" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
