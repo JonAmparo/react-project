699a18440706e565e165efd88069b9278524d4dd
jonamparo <jonathanmamparo@gmail.com>
2019-10-05 21:50:23 -0400

    news completed

---
+++ news/client/src/components/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  state = { stories: [] };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/topstories")
+      .then(response => response.json())
+      .then(json => this.setState({ stories: json }))
+      .catch(error => alert(error.message));
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>The Latest Hacker News</h2>
+        {this.state.stories.map(item => {
+          const { id, by, score, title, url, time } = item;
+
+          return (
+            <div key={id}>
+              <a href={url}>
+                <h3>{title}</h3>
+              </a>
+              <p>Upvotes: {score}</p>
+              {/* <p>{by}</p> */}
+              <p>
+                {by} - {new Date(time).toLocaleTimeString()}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ news/index.js
@@ -0,0 +1,82 @@
+const express = require("express");
+const request = require("request");
+const path = require("path");
+const stories = require("./stories");
+
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(
+    "Request details. Method:",
+    req.method,
+    "Original url:",
+    req.originalUrl
+  );
+
+  next();
+});
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+
+  next();
+});
+
+// essential for hosting on local and on a cloud!!
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
+app.get("/stories", (req, res) => {
+  res.json(stories);
+});
+
+app.get("/stories/:title", (req, res) => {
+  const { title } = req.params;
+
+  res.json(stories.filter(story => story.title.includes(title)));
+});
+
+app.get("/topstories", (req, res, next) => {
+  request(
+    { url: "https://hacker-news.firebaseio.com/v0/topstories.json" },
+    (error, response, body) => {
+      if (error || response.statusCode !== 200) {
+        return next(new Error("Error requesting top stories"));
+      }
+      const topStories = JSON.parse(body);
+      const limit = 10;
+
+      Promise.all(
+        topStories.slice(0, limit).map(story => {
+          return new Promise((resolve, reject) => {
+            request(
+              {
+                url: `https://hacker-news.firebaseio.com/v0/item/${story}.json`
+              },
+              (error, response, body) => {
+                if (error || response.statusCode !== 200) {
+                  return reject(new Error("Error requesting story item"));
+                }
+                resolve(JSON.parse(body));
+              }
+            );
+          });
+        })
+      )
+        .then(fullTopStories => {
+          res.json(fullTopStories);
+        })
+        .catch(error => next(error));
+    }
+  );
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({ type: "error", message: err.message });
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
